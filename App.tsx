439cadc57db78661809a60823284b9a9ef0a5b92
giuliabferreira <giuliabferreira@hotmail.com>
2023-08-31 08:57:40 -0300

    .

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Confeitaria from './src/pages/Confeitaria';
+
+export default function App() {
+  return <>
+  
+  </>
+
+};
+
+
