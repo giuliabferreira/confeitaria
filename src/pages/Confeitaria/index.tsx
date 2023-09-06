704c39a0d81f7bd51073d6f672c0792847b8b33e
giuliabferreira <giuliabferreira@hotmail.com>
2023-09-06 15:39:42 -0300

    .

--- src/pages/Confeitaria/index.tsx
+++ src/pages/Confeitaria/index.tsx
@@ -8,14 +8,9 @@ const width = Dimensions.get('screen').width;
 export default function Confeitaria() {
     return <>
     <Image source={topo} style={estilos.topo}/>
-    <Text style={estilos.titulo}>Detalhes do Carrinho</Text>
+    <Text style={estilos.titulo}>Detalhes da Confeitaria</Text>
     <View>
-        <Text style={estilos.nome}>Carrinho de Compras</Text>
-
-        <View>
-            <Image source={logo}/>
-        <Text style={estilos.nomeFazenda}>Borcelle Confeitaria</Text>
-        </View>
+        <Text style={estilos.nome}>Borcelle Confeitaria</Text>
 
         <Text style={estilos.descricao}>Nesta cesta está cheio de doces da nossa carinhosa confeitaria!</Text>
 
@@ -34,12 +29,12 @@ const estilos = StyleSheet.create({
     titulo: {
         width: "100%",
         position: "absolute",
-        color: "blue",
+        color: "white",
         textAlign: "center",
-        fontSize: 20,
+        fontSize: 23,
         lineHeight: 25,
-        padding: 15,
-        fontFamily: "Poppins400"
+        padding: 30,
+        fontFamily: "Poppins600"
     },
 
     carrinho: {
@@ -48,38 +43,24 @@ const estilos = StyleSheet.create({
     },
 
     nome: {
-        fontSize: 26,
-        lineHeight: 42,
+        fontSize: 19,
+        lineHeight: 40,
         color: "#464646",
-        fontFamily: "Poppins600"
-    },
-
-    fazenda: {
-        flexDirection: "row",
-        paddingVertical: 12
-    },
-
-    imagemFazenda: {
-        width: 32,
-        height: 32
-    },
-
-    nomeFazenda: {
-        fontSize: 16,
         fontFamily: "Poppins600",
-        lineHeight: 26,
-        marginLeft: 12
+        textAlign: "center",
+        padding: 10
     },
 
     descricao: {
-        color: "#A3A3A3",
-        fontFamily: "Poppins300"
+        color: "black",
+        fontFamily: "Poppins600",
+        textAlign: "justify"
     },
 
     preco: {
-        fontSize: 26,
-        fontFamily: "Poppins300",
-        color: "#2A9F85",
+        fontSize: 20,
+        fontFamily: "Poppins600",
+        color: "green",
         marginTop: 8
     }
     
