82276c11ecd5c92df1ccd29360ec1c673317f56f
giuliabferreira <giuliabferreira@hotmail.com>
2023-09-06 15:16:04 -0300

    .

--- App.tsx
+++ App.tsx
@@ -1,12 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { SafeAreaView, StatusBar } from 'react-native';
 import Confeitaria from './src/pages/Confeitaria';
+import { useFonts,
+  Poppins_300Light,
+  Poppins_400Regular,
+  Poppins_600SemiBold
+
+} from '@expo-google-fonts/poppins';
 
 export default function App() {
-  return <>
-  
-  </>
+  const [fonteCarregada] = useFonts ({
+    "Poppins300" : Poppins_300Light,
+    "Poppins400" : Poppins_400Regular,
+    "Poppins600": Poppins_600SemiBold
 
-};
+  })
 
+  if (!fonteCarregada) {
+    return null;
+  }
 
+  return (
+    <SafeAreaView>
+      <StatusBar/>
+      <Confeitaria/>
+    </SafeAreaView>
+  );
+
+}
--- src/pages/Confeitaria/index.tsx
+++ src/pages/Confeitaria/index.tsx
@@ -1,5 +1,86 @@
+import React from "react";
+import {  Dimensions, Image, StyleSheet, Text, View } from 'react-native';
+import topo from '../../../assets/header.jpg';
+import logo from '../../../assets/logo.webp';
+
+const width = Dimensions.get('screen').width;
+
 export default function Confeitaria() {
     return <>
-    
+    <Image source={topo} style={estilos.topo}/>
+    <Text style={estilos.titulo}>Detalhes do Carrinho</Text>
+    <View>
+        <Text style={estilos.nome}>Carrinho de Compras</Text>
+
+        <View>
+            <Image source={logo}/>
+        <Text style={estilos.nomeFazenda}>Borcelle Confeitaria</Text>
+        </View>
+
+        <Text style={estilos.descricao}>Nesta cesta está cheio de doces da nossa carinhosa confeitaria!</Text>
+
+            <Text style={estilos.preco}>R$90,00</Text>
+    </View>
     </>
+
 }
+
+const estilos = StyleSheet.create({
+    topo: {
+        width: "100%",
+        height: 578 / 768 * width,
+    },
+
+    titulo: {
+        width: "100%",
+        position: "absolute",
+        color: "blue",
+        textAlign: "center",
+        fontSize: 20,
+        lineHeight: 25,
+        padding: 15,
+        fontFamily: "Poppins400"
+    },
+
+    carrinho: {
+        paddingVertical: 8,
+        paddingHorizontal: 15
+    },
+
+    nome: {
+        fontSize: 26,
+        lineHeight: 42,
+        color: "#464646",
+        fontFamily: "Poppins600"
+    },
+
+    fazenda: {
+        flexDirection: "row",
+        paddingVertical: 12
+    },
+
+    imagemFazenda: {
+        width: 32,
+        height: 32
+    },
+
+    nomeFazenda: {
+        fontSize: 16,
+        fontFamily: "Poppins600",
+        lineHeight: 26,
+        marginLeft: 12
+    },
+
+    descricao: {
+        color: "#A3A3A3",
+        fontFamily: "Poppins300"
+    },
+
+    preco: {
+        fontSize: 26,
+        fontFamily: "Poppins300",
+        color: "#2A9F85",
+        marginTop: 8
+    }
+    
+})
